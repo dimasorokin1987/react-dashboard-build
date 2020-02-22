0c8c2e78e430ba1210984e9d3bf81dc3fa42cf74
Dima <Dima@Ilyas-MacBook-Air.local>
2020-02-22 17:24:11 +0200

    first commit to build repository

---
+++ precache-manifest.9d966351b31a20fc5fcce8af2067aa2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b51e63a6826a39c8635fac07d962337",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64b2b3c7a69e4ef50e1",
+    "url": "/static/css/main.29830384.chunk.css"
+  },
+  {
+    "revision": "69ee267ecc97ecb17fcc",
+    "url": "/static/js/2.8cf1f2f0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8cf1f2f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64b2b3c7a69e4ef50e1",
+    "url": "/static/js/main.57b95402.chunk.js"
+  },
+  {
+    "revision": "12f48908160f9b88711f",
+    "url": "/static/js/runtime-main.efc0802c.js"
+  }
+]);
